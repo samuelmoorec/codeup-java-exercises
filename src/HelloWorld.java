b8e38f904a6d5a4b9a477bccb903c832d7243443
arleneamericamartinez <hubacek1221>
2019-05-13 14:35:13 -0500

    finished exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,22 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        int myFavoriteNumber = 11;
+        String myString = "My Favorite number would have to be ";
+        double myNumber = 3.14;
+        int x = 4 + 5;
+        int a = 3;
+        int b = 4 * a;
+
+        int c = 10;
+        int d = 2;
+        c = c / d;
+        d = d - c;
+
+
+        System.out.println(myString + myFavoriteNumber);
+        System.out.println(myNumber);
+        System.out.println("Hello, World!");
+    }
+
+}
